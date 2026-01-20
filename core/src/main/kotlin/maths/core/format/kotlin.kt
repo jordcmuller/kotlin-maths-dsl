759e82bbfbee5b9f478eb17aee8d76de66539567
jordcmuller <jordan.c.muller13@gmail.com>
2026-01-20 20:14:00 +0200

    Updated printKotlin functions

--- core/src/main/kotlin/maths/core/format/kotlin.kt
+++ core/src/main/kotlin/maths/core/format/kotlin.kt
@@ -2,22 +2,22 @@ package maths.core.format
 
 import maths.core.ast.*
 
-// LaTeX-friendly output
+// kotlin syntax output
 fun Expr.printKotlin(): String = when (this) {
     is Const -> {
         val intValue = value.toInt()
         if (value == intValue.toDouble()) intValue.toString() else value.toString()
     }
     is Var -> name
-    is Add -> "${left.latex()} + ${right.latex()}"
-    is Mul -> "${left.latex()} * ${right.latex()}"
-    is Div -> "${left.latex()} / ${right.latex()}"
+    is Add -> "${left.printKotlin()} + ${right.printKotlin()}"
+    is Mul -> "${left.printKotlin()} * ${right.printKotlin()}"
+    is Div -> "${left.printKotlin()} / ${right.printKotlin()}"
     is Sub -> TODO()
+    is Pow -> "{${base.printKotlin()}} pow {${exp.printKotlin()}}" // what to do when squared?
     is BinaryExpr -> TODO()
-    is Pow -> "{${base.latex()}} pow {${exp.latex()}}" // what to do when squared?
     is Func -> TODO()
     is Neg -> TODO()
     else -> TODO()
 }
 
-fun Equation.printKotlin(): String = "${left.latex()} eq ${right.latex()}"
+fun Equation.printKotlin(): String = "${left.printKotlin()} eq ${right.printKotlin()}"
