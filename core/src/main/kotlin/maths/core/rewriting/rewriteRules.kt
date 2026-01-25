926bcb316d1bf7ef9c506a649ebbcc0a51d289a5
jordcmuller <jordan.c.muller13@gmail.com>
2026-01-25 21:40:01 +0200

    Created the additive and multiplicative identity rewrite rules

--- core/src/main/kotlin/maths/core/rewriting/rewriteRules.kt
+++ core/src/main/kotlin/maths/core/rewriting/rewriteRules.kt
@@ -5,6 +5,7 @@ import maths.core.ast.BinaryExpr
 import maths.core.ast.Mul
 import maths.core.verification.AnyNode
 import maths.core.verification.BinaryMatcher
+import maths.core.verification.ConstMatcher
 
 
 val multiplicativeCommutativity = RewriteRule(BinaryMatcher(AnyNode, MUL, AnyNode)) {
@@ -16,6 +17,10 @@ val multiplicativeAssociativity = RewriteRule(BinaryMatcher(BinaryMatcher(AnyNod
     else if (it.left !is BinaryExpr || it.left.operation != MUL) null
     else Mul(it.left.left, Mul(it.left.right, it.right))
 }
+val multiplicativeIdentity = RewriteRule(BinaryMatcher(AnyNode, MUL, ConstMatcher(1.0))) {
+    if (it !is BinaryExpr || it.operation != MUL) null
+    else it.left
+}
 
 val additiveCommutativity = RewriteRule(BinaryMatcher(AnyNode, ADD, AnyNode)) {
     if (it !is BinaryExpr || it.operation != ADD) null
@@ -25,5 +30,9 @@ val additiveAssociativity = RewriteRule(BinaryMatcher(BinaryMatcher(AnyNode, ADD
     if (it !is BinaryExpr || it.operation != ADD) null
     else if (it.left !is BinaryExpr || it.left.operation != ADD) null
     else Add(it.left.left, Add(it.left.right, it.right))
+}
+val additiveIdentity = RewriteRule(BinaryMatcher(AnyNode, ADD, ConstMatcher(0.0))) {
+    if (it !is BinaryExpr || it.operation != ADD) null
+    else it.left
 }
 
