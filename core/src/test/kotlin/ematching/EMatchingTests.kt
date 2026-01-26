c5e5ba0f3c7cded4e81edbf75601819cda211c4d
jordcmuller <jordan.c.muller13@gmail.com>
2026-01-26 23:17:43 +0200

    Added the scaffolding for the ematching tests

---
+++ core/src/test/kotlin/ematching/EMatchingTests.kt
@@ -0,0 +1,15 @@
+package ematching
+
+import io.kotest.core.spec.style.StringSpec
+
+class EMatchingTests : StringSpec({
+    "AnyNode should match all nodes" {
+
+    }
+    "AnyNode should not cause an infinite loop" {}
+    "AnyConst should match all constants" {}
+    "AnyVar should match all variables" {}
+    "ConstMatcher" {}
+    "VarMatcher" {}
+    "BinaryMatcher" {}
+})
