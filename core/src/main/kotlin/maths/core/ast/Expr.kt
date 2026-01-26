1a9b4d3677a223402e8984c86420a61052e80163
jordcmuller <jordan.c.muller13@gmail.com>
2026-01-26 23:06:26 +0200

    Added matches as a convenience method to the Expr interface

--- core/src/main/kotlin/maths/core/ast/Expr.kt
+++ core/src/main/kotlin/maths/core/ast/Expr.kt
@@ -1,9 +1,12 @@
 package maths.core.ast
 
+import maths.patterns.ExprPattern
 import kotlin.reflect.KProperty
 
 interface Expr {
     operator fun getValue(thisRef: Any?, property: KProperty<*>): Var {
         return Var(property.name) // improve on this
     }
+
+    infix fun matches(expr: Expr) = ExprPattern.fromExpr(this).accepts(expr)
 }
