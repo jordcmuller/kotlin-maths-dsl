58229858e0052e533e3e218b64e44896e8d2e1e4
jordcmuller <jordan.c.muller13@gmail.com>
2026-02-07 16:08:44 +0200

    Rewrote the eMatching algorithm to improve readability and simplicity

--- core/src/main/kotlin/maths/core/verification/EMatching.kt
+++ core/src/main/kotlin/maths/core/verification/EMatching.kt
@@ -1,135 +1,52 @@
 package maths.core.verification
 
-import maths.core.ast.BinaryExpr
-import maths.core.ast.Const
 import maths.core.ast.Expr
-import maths.core.ast.Operation
-import maths.core.ast.Var
-import maths.core.dsl.c
-import maths.core.dsl.plus
-import maths.core.dsl.v
-import maths.core.rewriting.additiveAssociativity
-import maths.core.rewriting.additiveCommutativity
 
-fun eMatch(eGraph: EGraph<Expr>, exprToFind: Expr): List<EMatchResult> {
-    return eMatch(eGraph, exprToFind.toEMatcher())
-}
-
-fun eMatch(eGraph: EGraph<Expr>, eMatcher: EMatcher, specificEClasses: List<EClassId> = emptyList()): List<EMatchResult> {
-    // The goal of eMatch is to find all the expressions that match the input expression and return them as a list
-    // I am visualising this as zipping the incoming expression onto all top level expressions that are represented in the egraph.
-    // TODO: should we also match sub expressions. Commutativity matches `a+b` and `(...)+c` in the expression `(a+b)+c`
-    // I believe that the way this is being done below will actually account for the subexpressions since it starts searching through all eclasses
-
-    // the algorithm will be something along the lines of
-    // 1. what is the root element in the expression? Find all eClasses that contain it.
-    // 2. find all enodes in the eclasses that match the element, including matching number of children
-    // 3. For each matching enode, ematch each child expr against the eclass that is the operand of the enode, in order of children.
-    // 3.1 If all children match, then create an expression from the results returned in the child ematching and add it to the output list
-    // 4. If the expr has no children then it is terminal and any matches will be added to the output list
-    // 5. If no enodes match then an empty list is returned.
-    // Finally, return the output list
-
-    val nodesToSearch =
-        if (specificEClasses.isNotEmpty()) specificEClasses.mapNotNull { eGraph.eClassesById[it] }.flatMap { it.nodes }
-        else eGraph.eNodes
-
-    val allMatchedExpressions = nodesToSearch
-        .filter { identifierMatch(it, eMatcher) }
-        .filter { childCountMatch(it, eMatcher) }
-        .flatMap { eNode ->
-            if (eMatcher is AnyNode) return@flatMap listOf(EMLeaf(eGraph.eNodeHashCons[eNode.toHashKey]!!))
-            if (eNode.children.isEmpty()) return@flatMap listOf(EMLeaf(eGraph.eNodeHashCons[eNode.toHashKey]!!))
-
-            val allMatchedExpressionsForEachChild = eNode.children.zip(eMatcher.children)
-                .map { (child, childMatcher) -> eMatch(eGraph, childMatcher, listOf(child)) }
-
-            val firstChildMatchedExpressions = allMatchedExpressionsForEachChild.first()
-            val cartesianProductStarter = firstChildMatchedExpressions.map(::listOf)
-
-            val allPossibleChildCombos = allMatchedExpressionsForEachChild.drop(1)
-                .fold(cartesianProductStarter) { acc, childMatchedExpressions ->
-                    val output = mutableListOf<List<EMatchResult>>()
-
-                    acc.forEach { childrenCombo ->
-                        childMatchedExpressions.forEach { childExpr ->
-                            output += childrenCombo + childExpr
-                        }
-                    }
-
-                    return@fold output
-                }
-
-            return@flatMap allPossibleChildCombos.map { childCombo -> EMBinary(eGraph.eNodeHashCons[eNode.toHashKey]!!, childCombo[0], getOperation(eNode.identifier), childCombo[1]) }
-        }
-
-    return allMatchedExpressions
-}
-
-fun identifierMatch(eNode: ENode, eMatcher: EMatcher): Boolean {
-    return when (eMatcher) {
-        is AnyNode -> true
-        is AnyConst -> eNode is EConst
-        is AnyVar -> eNode is EVar
-        is ConstMatcher -> eNode.identifier == eMatcher.value.toString()
-        is VarMatcher -> eNode.identifier == eMatcher.name
-        is BinaryMatcher -> eNode.identifier == eMatcher.operation.symbol
-    }
-}
-
-fun childCountMatch(it: ENode, eMatcher: EMatcher) = when (eMatcher) {
+infix fun EMatcher.idMatches(identifier: String) = when (this) {
     is AnyNode -> true
-    else -> it.children.size == eMatcher.children.size
+    is BinaryMatcher -> operations.any { it.symbol == identifier }
+    else -> TODO("matchId not implemented yet for $this")
 }
 
-fun Expr.toEMatcher(): EMatcher {
-    return when (this) {
-        is Const -> ConstMatcher(value)
-        is Var -> VarMatcher(name)
-        is BinaryExpr -> BinaryMatcher(left.toEMatcher(), operation, right.toEMatcher())
-        else -> TODO("EMatcher mapping for $javaClass not supported yet")
+infix fun EMatcher.childrenCountMatches(size: Int) = when (this) {
+    is AnyNode -> true
+    else -> children.size == size
+}
+
+fun EGraph<Expr>.eMatch(eMatcher: EMatcher, nodesToSearch: List<ENode> = eNodes): List<EMatchResult> {
+    return nodesToSearch
+        .filter { eMatcher idMatches it.identifier }
+        .filter { eMatcher childrenCountMatches it.children.size }
+        .flatMap { getMatchResults(eMatcher, it) }
+}
+
+fun EGraph<Expr>.getMatchResults(matcher: EMatcher, node: ENode): List<EMatchResult> {
+    val nodeEClassId = eNodeHashCons[node.toHashKey] ?: return emptyList()
+
+    return when (matcher) {
+        is AnyNode -> listOf(EMatchResult(nodeEClassId, mapOf(matcher.name to nodeEClassId)))
+        is BinaryMatcher -> {
+            val leftEClass = eClassesById[node.children[0]] ?: return emptyList()
+            val rightEClass = eClassesById[node.children[1]] ?: return emptyList()
+
+            val leftResults = eMatch(matcher.left, leftEClass.nodes).ifEmpty { return emptyList() }
+            val rightResults = eMatch(matcher.right, rightEClass.nodes).ifEmpty { return emptyList() }
+
+            val combinedConsistentMatches = leftResults combineWith rightResults
+            combinedConsistentMatches.map { EMatchResult(nodeEClassId, it) }
+        }
+        else -> TODO("matchChildren not implemented yet for $matcher")
     }
 }
 
-sealed class EMatcher(val children: List<EMatcher> = emptyList())
-data object AnyVar: EMatcher()
-data object AnyConst: EMatcher()
-data object AnyNode: EMatcher()
-data class ConstMatcher(val value: Double) : EMatcher()
-data class VarMatcher(val name: String) : EMatcher()
-data class BinaryMatcher(val left: EMatcher, val operation: Operation, val right: EMatcher) : EMatcher(listOf(left, right))
+infix fun List<EMatchResult>.combineWith(otherList: List<EMatchResult>): List<Map<String, EClassId>> {
+    return flatMap { first -> otherList.map { second -> first to second } }
+        .filter { consistentMatches(it.first.matchedGroups, it.second.matchedGroups) }
+        .map { it.first.matchedGroups + it.second.matchedGroups }
+}
 
-operator fun EMatcher.plus(that: EMatcher) = BinaryMatcher(this, ADD, that)
-operator fun EMatcher.times(that: EMatcher) = BinaryMatcher(this, MUL, that)
-
-sealed class EMatchResult(val eClassId: EClassId)
-data class EMLeaf(val id: EClassId) : EMatchResult(id)
-data class EMBinary(val id: EClassId, val left: EMatchResult, val operation: Operation, val right: EMatchResult) : EMatchResult(id)
-
-sealed interface RewriteResult
-data class RRLeaf(val eClassId: EClassId) : RewriteResult
-data class RRBinary(val left: RewriteResult, val operation: Operation, val right: RewriteResult) : RewriteResult
-
-
-fun main() {
-    val egg = MathsEGraph()
-
-    egg.add("x".v + 1.c)
-
-    val rewriteRules = listOf(additiveCommutativity, additiveAssociativity)
-    saturate(egg, rewriteRules)
-
-    println(eMatch(egg, 1.c))
-    println(eMatch(egg, "x".v + 1.c))
-    println(eMatch(egg, 1.c + "x".v))
-    println(eMatch(egg, BinaryMatcher(AnyNode, ADD, AnyNode)))
-
-    println("Associativity")
-
-    egg.add("a".v + "b".v + "c".v)
-    println(eMatch(egg, "a".v + "b".v + "c".v))
-
-    saturate(egg, rewriteRules)
-    println(eMatch(egg, "a".v + ("b".v + "c".v)))
-
+fun consistentMatches(firstMap: Map<String, EClassId>, secondMap: Map<String, EClassId>): Boolean {
+    return secondMap.all { (matchName, eClassId) ->
+        firstMap.containsKey(matchName) && firstMap[matchName] != eClassId
+    }
 }
