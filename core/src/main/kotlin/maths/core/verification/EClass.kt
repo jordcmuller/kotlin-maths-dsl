a2eb4b01fa2cc6331fd905f9f668b5929354c70b
jordcmuller <jordan.c.muller13@gmail.com>
2026-02-07 20:28:40 +0200

    Changed EClass to be a normal class and added overrides for toString, equals, and hashcode to fix map indexing issues

--- core/src/main/kotlin/maths/core/verification/EClass.kt
+++ core/src/main/kotlin/maths/core/verification/EClass.kt
@@ -2,7 +2,22 @@ package maths.core.verification
 
 typealias EClassId = Int
 
-data class EClass(
+class EClass(
     val id: EClassId,
     val nodes: MutableList<ENode> = mutableListOf(),
-)
+) {
+    override fun hashCode() = id
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as EClass
+
+        return id == other.id
+    }
+
+    override fun toString(): String {
+        return "First Node: ${nodes.first()}"
+    }
+}
