b6c41ebfbf92d417db85ada931095b542f07e5e8
jordcmuller <jordan.c.muller13@gmail.com>
2026-02-10 23:16:50 +0200

    Added more to the example

--- core/src/main/kotlin/examples/example1.kt
+++ core/src/main/kotlin/examples/example1.kt
@@ -1,7 +1,9 @@
 package examples
 
 import maths.core.dsl.maths
+import maths.core.dsl.minus
 import maths.core.dsl.plus
+import maths.core.dsl.unaryMinus
 
 fun main() {
     maths {
@@ -10,13 +12,12 @@ fun main() {
 
         if (x notEqual y) println("These are different variables, of course they're not equal")
 
-        val expr1 = y + 1
-        val expr2 = x + 1
+        if (y + 1 notEqual x + 1) println("And these expressions are also not equal...")
 
-        if (expr1 notEqual expr2) println("These expressions are also not equal...")
+        if (x + 1 - y equal -y + (1 + x)) println("But this equation is true thanks to the properties of addition!")
 
         x equate y
 
-        if (expr1 equal expr2) println("But now they are!")
+        if (y + 1 equal x + 1) println("After equating x and y, the previous equation is now true!")
     }
 }
