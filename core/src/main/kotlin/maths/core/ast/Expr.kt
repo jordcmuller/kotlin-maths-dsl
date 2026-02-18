eb5b89b1d5557f3e11d11735497778dc02e4036f
jordcmuller <jordan.c.muller13@gmail.com>
2026-02-18 20:51:25 +0200

    Removed unused delegate

--- core/src/main/kotlin/maths/core/ast/Expr.kt
+++ core/src/main/kotlin/maths/core/ast/Expr.kt
@@ -1,12 +1,7 @@
 package maths.core.ast
 
 import maths.patterns.ExprPattern
-import kotlin.reflect.KProperty
 
 interface Expr {
-    operator fun getValue(thisRef: Any?, property: KProperty<*>): Var {
-        return Var(property.name) // improve on this
-    }
-
     infix fun matches(expr: Expr) = ExprPattern.fromExpr(this).accepts(expr)
 }
