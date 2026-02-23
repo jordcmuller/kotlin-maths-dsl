d45a68f5572914a3f6643ef1610ef0e824f41e71
jordcmuller <jordan.c.muller13@gmail.com>
2026-02-23 20:24:31 +0200

    Tidied up e-matching

--- core/src/main/kotlin/maths/core/verification/EMatchResult.kt
+++ core/src/main/kotlin/maths/core/verification/EMatchResult.kt
@@ -2,6 +2,6 @@ package maths.core.verification
 
 import maths.core.ast.Expr
 
-class EMatchResult(val rootEClass: EClass, val matchedExpressions: Map<String, Expr> = emptyMap()) {
-    operator fun get(anyNode: AnyNode) = matchedExpressions[anyNode.name] ?: error("matched group ${anyNode.name} not found")
+class EMatchResult(val matchedExpressions: Map<String, Expr> = emptyMap()) {
+    operator fun get(anyNode: AnyNode) = matchedExpressions[anyNode.name]
 }
--- core/src/main/kotlin/maths/core/verification/EMatching.kt
+++ core/src/main/kotlin/maths/core/verification/EMatching.kt
@@ -82,6 +82,18 @@ fun EMatcher.fillInKnownVariables(result: EMatchResult): EMatcher = when (this) 
     else -> TODO()
 }
 
+fun EGraph.eMatch(eMatcher: EMatcher, nodesToSearch: List<ENode> = eNodes): List<EMatchResult> {
+    if (eMatcher is AnyNode)
+        return nodesToSearch
+            .map { it.parentEClass }.distinct()
+            .map { EMatchResult(mapOf(eMatcher.name to it.canonicalForm)) }
+
+    return nodesToSearch
+        .filter { eMatcher idMatches it.identifier }
+        .filter { eMatcher childrenCountMatches it.childEClasses.size }
+        .flatMap { getMatchResults(eMatcher, it) }
+}
+
 infix fun EMatcher.idMatches(identifier: String) = when (this) {
     is AnyNode -> true
     is ConstMatcher -> value.toString() == identifier
@@ -96,26 +108,17 @@ infix fun EMatcher.childrenCountMatches(size: Int) = when (this) {
     else -> children.size == size
 }
 
-fun EGraph.eMatch(eMatcher: EMatcher, nodesToSearch: List<ENode> = eNodes): List<EMatchResult> {
-    return nodesToSearch
-        .filter { eMatcher idMatches it.identifier }
-        .filter { eMatcher childrenCountMatches it.childEClasses.size }
-        .flatMap { getMatchResults(eMatcher, it) }
-}
-
 fun EGraph.getMatchResults(matcher: EMatcher, node: ENode): List<EMatchResult> {
     val nodeEClass = eNodeHashCons[node.toHashKey] ?: return emptyList()
     val nodeCanonicalForm = nodeEClass.canonicalForm
 
     return when (matcher) {
-        is AnyNode -> listOf(EMatchResult(nodeEClass, mapOf(matcher.name to nodeCanonicalForm)))
-        is ConstMatcher -> listOf(EMatchResult(nodeEClass))
+        is AnyNode -> listOf(EMatchResult(mapOf(matcher.name to nodeCanonicalForm)))
+        is ConstMatcher, is VarMatcher -> listOf(EMatchResult())
         is UnaryMatcher -> {
             val operandEClass = node.childEClasses[0]
 
-            val operandResults = eMatch(matcher.operand, operandEClass.nodes).ifEmpty { return emptyList() }
-
-            operandResults.map { EMatchResult(nodeEClass, it.matchedExpressions) }
+            eMatch(matcher.operand, operandEClass.nodes).ifEmpty { return emptyList() }
         }
         is BinaryMatcher -> {
             val leftEClass = node.childEClasses[0]
@@ -125,7 +128,7 @@ fun EGraph.getMatchResults(matcher: EMatcher, node: ENode): List<EMatchResult> {
             val rightResults = eMatch(matcher.right, rightEClass.nodes).ifEmpty { return emptyList() }
 
             val combinedConsistentMatches = leftResults combineWith rightResults
-            combinedConsistentMatches.map { EMatchResult(nodeEClass, it) }
+            combinedConsistentMatches.map { EMatchResult(it) }
         }
         else -> TODO("getMatchResults not implemented yet for $matcher")
     }
