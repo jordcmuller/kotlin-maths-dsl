74526849b2c22fd99d3635ed3db54075a2e1cabe
jordcmuller <jordan.c.muller13@gmail.com>
2026-04-11 01:58:12 +0200

    Improved readability of test

--- core/src/test/kotlin/analysis/VectorAnalysisDataTest.kt
+++ core/src/test/kotlin/analysis/VectorAnalysisDataTest.kt
@@ -3,30 +3,36 @@ package analysis
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 import maths.core.ast.Const
+import maths.core.ast.Expr
 import maths.core.dsl.MathsContext
 import maths.core.dsl.plus
 
 class VectorAnalysisDataTest: StringSpec({
 
     data class VectorTest(val x: Int, val y: Int)
+    fun vector(x: Int, y: Int) = Const(VectorTest(x, y))
+
 
     "Vector addition carries forward to compound operations" {
         MathsContext.empty {
+            infix fun Expr.shouldEqual(other: Expr) = this equal other shouldBe true
+            infix fun Expr.shouldNotEqual(other: Expr) = this equal other shouldBe false
+
             withOperation("+") { a: VectorTest, b: VectorTest -> VectorTest(a.x + b.x, a.y + b.y) }
 
             val x by variable()
             val y by variable()
 
-            x equate Const(VectorTest(1, 4))
+            x equate vector(1, 4)
 
-            x equal Const(VectorTest(1, 4)) shouldBe true
-            x + y equal Const(VectorTest(1, 4)) + y shouldBe true
+            x shouldEqual vector(1, 4)
+            x + y shouldEqual vector(1, 4) + y
 
-            y equate Const(VectorTest(10, 10))
+            y equate vector(10, 10)
 
-            y equal Const(VectorTest(10, 10)) shouldBe true
-            x + y equal Const(VectorTest(11, 14)) shouldBe true
-            x + y equal Const(VectorTest(11, 15)) shouldBe false
+            y shouldEqual vector(10, 10)
+            x + y shouldEqual vector(11, 14)
+            x + y shouldNotEqual vector(11, 15)
         }
     }
 })
